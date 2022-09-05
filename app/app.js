93dfcde3143a7b85174cf12c70c5f74a86b87f98
John Sylvester <john@sly.dev>
2022-09-05 12:26:33 +0100

    Remove debug console.log message

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (request, response) => {
-	console.log("HERE!!!");
 	response.status(200).send({ message: "Server is up" });
 });
 
