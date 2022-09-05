818b6c62fcaee01c74bc9dcfc45afedbb6b2694d
John Sylvester <john@sly.dev>
2022-09-05 15:12:09 +0100

    Call selectArticleById function and pass in the article_id from the request params

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -1,3 +1,12 @@
+const { selectArticleById } = require("../models/articles.models");
+
 exports.getArticleById = (request, response, next) => {
-	response.status(200).send({ article: {} });
+	const article_id = request.params.article_id;
+	selectArticleById(article_id)
+		.then((article) => {
+			response.status(200).send({ article });
+		})
+		.catch((error) => {
+			next(error);
+		});
 };
