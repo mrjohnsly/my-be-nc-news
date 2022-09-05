594c42998bee24f03578ca134abcb638d21d1493
John Sylvester <john@sly.dev>
2022-09-05 12:17:08 +0100

    Add test to check the server is up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,3 +6,16 @@ const db = require("../db/connection.js");
 beforeAll(() => {
 	return seed();
 });
+
+describe("/api", () => {
+	describe("GET", () => {
+		test("200: Responds with a message 'Server is up'", () => {
+			return supertest(app)
+				.get("/api")
+				.expect(200)
+				.then((response) => {
+					expect(response).toBe({ message: "Server is up" });
+				});
+		});
+	});
+});
