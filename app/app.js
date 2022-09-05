3a4142e547c9fd88914f0123a6d200756556b332
John Sylvester <john@sly.dev>
2022-09-05 12:41:20 +0100

    Add GET /api/topics and return an empty array

--- app/app.js
+++ app/app.js
@@ -6,4 +6,8 @@ app.get("/api", (request, response) => {
 	response.status(200).send({ message: "Server is up" });
 });
 
+app.get("/api/topics", (request, response) => {
+	response.status(200).send({ topics: [] });
+});
+
 module.exports = app;
