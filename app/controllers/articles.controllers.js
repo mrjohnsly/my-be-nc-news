cb46f2f8e9e60f58f3e7dc803729df3ccc862dd8
mrjohnsly <john@sly.dev>
2022-09-06 11:19:05 +0100

    Add logic to check the method of the request when calling the getArticleById controller

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -1,12 +1,16 @@
 const { selectArticleById } = require("../models/articles.models");
 
 exports.getArticleById = (request, response, next) => {
-	const article_id = request.params.article_id;
-	selectArticleById(article_id)
-		.then((article) => {
-			response.status(200).send({ article });
-		})
-		.catch((error) => {
-			next(error);
-		});
+	if (request.method === "GET") {
+		const article_id = request.params.article_id;
+		selectArticleById(article_id)
+			.then((article) => {
+				response.status(200).send({ article });
+			})
+			.catch((error) => {
+				next(error);
+			});
+	} else {
+		response.status(405).send({ error: { code: 405, message: "Method not allowed" } });
+	}
 };
