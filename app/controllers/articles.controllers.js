d14abfc8ec5c0c86a367b81ef9a45702ef512679
mrjohnsly <john@sly.dev>
2022-09-06 15:56:37 +0100

    Add logic to check if the body has the required inc_votes property

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -18,11 +18,16 @@ exports.getArticleById = (request, response, next) => {
 exports.patchArticleById = (request, response, next) => {
 	const article_id = request.params.article_id;
 	const numberOfVotes = request.body.inc_votes;
-	updateArticleById(article_id, numberOfVotes)
-		.then((article) => {
-			response.status(200).send({ article });
-		})
-		.catch((error) => {
-			next(error);
-		});
+
+	if (numberOfVotes) {
+		updateArticleById(article_id, numberOfVotes)
+			.then((article) => {
+				response.status(200).send({ article });
+			})
+			.catch((error) => {
+				next(error);
+			});
+	} else {
+		next({ code: 400, message: "Bad Request" });
+	}
 };
