8b313a6b182f511f201712560b3fc2f930927628
mrjohnsly <john@sly.dev>
2022-09-06 12:21:24 +0100

    Add catch block to pass any errors to the error handling middleware

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -4,5 +4,8 @@ exports.getUsers = (request, response, next) => {
 	selectUsers()
 		.then((users) => {
 			response.status(200).send({ users });
+		})
+		.catch((error) => {
+			next(error);
 		});
 };
