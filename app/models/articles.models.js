de30868ea5b835e95a44448d333e5847c8cd49ab
John Sylvester <john@sly.dev>
2022-09-07 11:17:01 +0100

    Add a db query to get the count of comments for an article

--- app/models/articles.models.js
+++ app/models/articles.models.js
@@ -1,16 +1,25 @@
 const db = require("../../db/connection.js");
 
 exports.selectArticleById = (article_id) => {
-	return db.query(`
+	const getArticle = db.query(`
 		SELECT * FROM articles
 		WHERE article_id = $1
-	`, [article_id])
-		.then((dbResult) => {
-			if (dbResult.rowCount === 0) {
+	`, [article_id]);
+
+	const getComments = db.query(`
+		SELECT COUNT(article_id) from comments
+		WHERE article_id = $1
+	`, [article_id]);
+
+	return Promise.all([getArticle, getComments])
+		.then((values) => {
+			if (values[0].rowCount === 0) {
 				return Promise.reject({ code: 404, message: "No article found" });
-			} else {
-				return dbResult.rows[0];
 			}
+
+			const article = values[0].rows[0];
+			article.comment_count = values[1].rows[0].count;
+			return article;
 		});
 };
 
