805e89d6162f66a4d281982dd8f66867d8432315
mrjohnsly <john@sly.dev>
2022-09-08 15:02:51 +0100

    Add tests to check articles returns with the correct properties and in descending order

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -64,6 +64,48 @@ describe("/api/topics", () => {
 	});
 });
 
+describe("/api/articles", () => {
+	describe("GET", () => {
+		test("200: Responds with an array of articles'", () => {
+			return supertest(app)
+				.get("/api/articles")
+				.expect(200)
+				.then(({ body }) => {
+					expect(Array.isArray(body.articles)).toBe(true);
+				});
+		});
+
+		test("200: Articles are sorted by created_at in descending order'", () => {
+			return supertest(app)
+				.get("/api/articles")
+				.expect(200)
+				.then(({ body }) => {
+					expect(body.articles).toBeSortedBy("created_at", {
+						descending: true
+					});
+				});
+		});
+
+		test("200: Each article has the correct properties'", () => {
+			return supertest(app)
+				.get("/api/articles")
+				.expect(200)
+				.then(({ body }) => {
+					expect(body.articles.length).toBeGreaterThan(0);
+					body.articles.forEach((article) => {
+						expect(article).toHaveProperty("author");
+						expect(article).toHaveProperty("title");
+						expect(article).toHaveProperty("article_id");
+						expect(article).toHaveProperty("topic");
+						expect(article).toHaveProperty("created_at");
+						expect(article).toHaveProperty("votes");
+						expect(article).toHaveProperty("comment_count");
+					});
+				});
+		});
+	});
+});
+
 describe("/api/articles/:article_id", () => {
 	describe("GET", () => {
 		test("200: Responds with an article object", () => {
