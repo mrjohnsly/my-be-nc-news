9cb44e7c872114cd1f77b7851180681eb8fd4c5f
mrjohnsly <john@sly.dev>
2022-09-09 13:55:57 +0100

    Refactor articles routes to use express.route

--- app/routers/articles.router.js
+++ app/routers/articles.router.js
@@ -2,11 +2,16 @@ const express = require("express");
 const { getArticles, getArticleById, patchArticleById, getArticleComments } = require("../controllers/articles.controllers");
 const articlesRouter = express.Router();
 
-articlesRouter.get("/", getArticles);
-articlesRouter.get("/:article_id", getArticleById);
-articlesRouter.post("/:article_id", getArticleById);
-articlesRouter.patch("/:article_id", patchArticleById);
-articlesRouter.delete("/:article_id", getArticleById);
-articlesRouter.get("/:article_id/comments", getArticleComments);
+articlesRouter.route("/")
+	.get(getArticles);
+
+articlesRouter.route("/:article_id")
+	.get(getArticleById)
+	.post(getArticleById)
+	.patch(patchArticleById)
+	.delete(getArticleById);
+
+articlesRouter.route("/:article_id/comments")
+	.get(getArticleComments);
 
 module.exports = articlesRouter;
