f1c89fb25c8cf30acb7031ff321cea387893e783
mrjohnsly <john@sly.dev>
2022-09-09 20:50:16 +0100

    Refactor error objects

--- app/middleware/error.middleware.js
+++ app/middleware/error.middleware.js
@@ -1,8 +1,8 @@
 exports.statusCodeError = (error, request, response, next) => {
 	if (error.code === 400) {
-		response.status(400).send({ error: error });
+		response.status(400).send({ error });
 	} else if (error.code === 404) {
-		response.status(404).send({ error: error });
+		response.status(404).send({ error });
 	} else if (error.code === 501) {
 		response.status(501).send({ error: { message: "501 Not Implemented" } });
 	} else {
