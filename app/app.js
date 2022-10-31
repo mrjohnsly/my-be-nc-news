2bdd7e40eca5d868f18ab1cecec09f3f850d9d11
John Sylvester <john@sly.dev>
2022-10-31 09:59:31 +0000

    Allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { statusCodeError, psqlError, unhandledError } = require("./middleware/error.middleware");
 const apiRouter = require("./routers/api.routers");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
